ea15251c81ab6a801e169b9301c3146013e9c40e
Zach <64901145+Zachareee@users.noreply.github.com>
2024-01-21 15:54:02 +0000

    Changed db implementation

--- TTVutils.js
+++ TTVutils.js
@@ -1,132 +1,141 @@
-import { createHmac, timingSafeEqual, randomBytes } from "crypto"
-import fetch from "node-fetch"
-import { exec } from "child_process"
-import Database from "@replit/database"
+import { createHmac, timingSafeEqual, randomBytes } from "crypto";
+import fetch from "node-fetch";
+import { Low } from "lowdb";
+import { JSONFile } from "lowdb/node";
 
-const db = new Database()
+const db = new Low(new JSONFile("./TTVdb.json"), {});
+db.read();
 
 export function verifyTTVHash() {
   return (req, res, buf, encoding) => {
-    const header = req.header('SHA1-Signature')
-    const sigBuffer = Buffer.from(header, "hex")
-    const hash = createHmac("sha1", process.env.TTVsecret).update(buf).digest()
+    const header = req.header("SHA1-Signature");
+    const sigBuffer = Buffer.from(header, "hex");
+    const hash = createHmac("sha1", process.env.TTVsecret).update(buf).digest();
     try {
       if (!timingSafeEqual(hash, sigBuffer)) {
-        throw new Error("Bad request signature")
+        throw new Error("Bad request signature");
       }
     } catch (err) {
-      console.error(err)
-      console.log(`Hash: ${hash.toString("hex")}, signature: ${header}`)
-      res.status(401).send("Bad request signature")
-      throw new Error("Bad request signature")
+      console.error(err);
+      console.log(`Hash: ${hash.toString("hex")}, signature: ${header}`);
+      res.status(401).send("Bad request signature");
+      throw new Error("Bad request signature");
     }
-  }
+  };
 }
 
 export function gen_state() {
-  const buffer = randomBytes(20)
-  const state = buffer.toString("hex")
-  db.set("state",state)
-  return state
+  const buffer = randomBytes(20);
+  const state = buffer.toString("hex");
+  db.update((data) => (data.state = state));
+  return state;
 }
 
 export async function check_state(test) {
-  return db.get("state").then(state => {
-    return state == test
-  })
+  return db.data.state === test;
 }
 
 export async function use_code(code) {
-  const body = `client_id=${process.env.TTVclientID}&` +
+  const body =
+    `client_id=${process.env.TTVclientID}&` +
     `client_secret=${process.env.TTVclientS}&` +
     `code=${code}&` +
     `grant_type=authorization_code&` +
-    `redirect_uri=${process.env.callback}ttv/auth`
-  const result = await fetch("https://id.twitch.tv/oauth2/token", { 
+    `redirect_uri=${process.env.callback}ttv/auth`;
+  const result = await fetch("https://id.twitch.tv/oauth2/token", {
     method: "POST",
     headers: {
-      "Content-Type": "application/x-www-form-urlencoded"
+      "Content-Type": "application/x-www-form-urlencoded",
     },
-    body: body
-  })
+    body: body,
+  });
 
-  saveToken(await result.json())
+  saveToken(await result.json());
 }
 
 export async function TTVinfo() {
-  const token = await tokenEval()
-  if (token == null) return null
+  const token = await tokenEval();
+  if (token == null) return null;
   const options = {
     method: "GET",
     headers: {
       "Client-Id": process.env.TTVclientID,
-      "Authorization": token
-    }
-  }
-  var endpoint = `https://api.twitch.tv/helix/channels?broadcaster_id=${process.env.TTVchannel}`
-  var result = await fetch(endpoint, options)
-  var response = await result.json()
-  const { title, game_name: game, game_id: ID } = response.data[0]
+      Authorization: token,
+    },
+  };
+  var endpoint = `https://api.twitch.tv/helix/channels?broadcaster_id=${process.env.TTVchannel}`;
+  var result = await fetch(endpoint, options);
+  var response = await result.json();
+  const { title, game_name: game, game_id: ID } = response.data[0];
 
-  endpoint = `https://api.twitch.tv/helix/games?id=${ID}`
-  result = await fetch(endpoint, options)
-  response = await result.json()
-  const { box_art_url: art } = response.data[0]
-  return { title: title, game: game, art: `${art.split("-{width}x{height}")[0]}.jpg`}
+  endpoint = `https://api.twitch.tv/helix/games?id=${ID}`;
+  result = await fetch(endpoint, options);
+  response = await result.json();
+  const { box_art_url: art } = response.data[0];
+  return {
+    title: title,
+    game: game,
+    art: `${art.split("-{width}x{height}")[0]}.jpg`,
+  };
 }
 
 export async function tokenEval() {
-  const token = await db.get("token")
+  const token = await db.data.token;
   const result = await fetch("https://id.twitch.tv/oauth2/validate", {
     method: "GET",
     headers: {
-      Authorization: token
-    }
-  })
-  console.log(await result.json())
+      Authorization: token,
+    },
+  });
+  console.log(await result.json());
 
-  if(result.status == 200) {
-    return token
+  if (result.status == 200) {
+    return token;
   }
 
-  return await refreshToken()
+  return await refreshToken();
 }
 
 function saveToken(data) {
-  const { access_token: access = null, refresh_token: refresh = null} = data
-  db.set("token", `Bearer ${access}`)
-  db.set("refresh", refresh)
+  const { access_token: access = null, refresh_token: refresh = null } = data;
+  db.update((data) => {
+    data.token = `Bearer ${access}`;
+  });
+  db.update((data) => {
+    data.refresh = refresh;
+  });
 }
 
 async function refreshToken() {
-  const refresh = await db.get("refresh")  
-  const body = `grant_type=refresh_token&` +
+  const refresh = db.data.refresh;
+  const body =
+    `grant_type=refresh_token&` +
     `refresh_token=${refresh}&` +
     `client_id=${process.env.TTVclientID}&` +
-    `client_secret=${process.env.TTVclientS}`
+    `client_secret=${process.env.TTVclientS}`;
   const result = await fetch("https://id.twitch.tv/oauth2/token", {
     method: "POST",
     headers: {
-      "Content-Type": "application/x-www-form-urlencoded"
+      "Content-Type": "application/x-www-form-urlencoded",
     },
-    body: body
-  })
+    body: body,
+  });
 
   if (result.status == 400) {
-    db.delete("refresh")
-    return null
+    db.update((data) => (data.refresh = null));
+    return null;
   }
-  const obj = await result.json()
-  saveToken(obj)
-  return `Bearer ${obj.access_token}`
+  const obj = await result.json();
+  saveToken(obj);
+  return `Bearer ${obj.access_token}`;
 }
 
 export class Err {
   static set(error) {
-    this.error = error
+    this.error = error;
   }
 
   static get() {
-    return this.error
+    return this.error;
   }
 }
--- YTutils.js
+++ YTutils.js
@@ -1,79 +1,84 @@
-import { createHmac, timingSafeEqual } from "crypto"
-import fetch from "node-fetch"
-import { exec } from "child_process"
-import Database from "@replit/database"
+import { createHmac, timingSafeEqual } from "crypto";
+import fetch from "node-fetch";
+import { exec } from "child_process";
+import { Low } from "lowdb";
+import { JSONFile } from "lowdb/node";
 
-const db = new Database()
+const db = new Low(new JSONFile("./YTdb.json"), {});
 
 export function verifyYTHash() {
   return (req, res, buf, encoding) => {
-    const header = req.header('X-Hub-Signature')
-    const signature = header.split("sha1=")[1]
-    const sigBuffer = Buffer.from(signature,"hex")
-    const hash = createHmac("sha1", process.env.YTsecret).update(buf).digest()
+    const header = req.header("X-Hub-Signature");
+    const signature = header.split("sha1=")[1];
+    const sigBuffer = Buffer.from(signature, "hex");
+    const hash = createHmac("sha1", process.env.YTsecret).update(buf).digest();
     try {
       if (!timingSafeEqual(hash, sigBuffer)) {
-        console.log(`${hash.toString("hex")} < hash\nsigBuffer.toString("hex")}`)
-        res.status(401).send("Bad request signature")
-        throw new Error("Bad request signature")
+        console.log(
+          `${hash.toString("hex")} < hash\nsigBuffer.toString("hex")}`,
+        );
+        res.status(401).send("Bad request signature");
+        throw new Error("Bad request signature");
       }
-    } catch(err) {
-        console.error(err)
-        res.status(401).send("Bad request signature")
-        throw new Error("Bad request signature")
+    } catch (err) {
+      console.error(err);
+      res.status(401).send("Bad request signature");
+      throw new Error("Bad request signature");
     }
-  }
+  };
 }
 
 export async function refresh(leaseSeconds) {
   if (leaseSeconds) {
-    db.set("lease", parseInt(leaseSeconds))
-    db.set("start", Date.now()/1000)
-    console.log("Returned")
-    return
+    db.set("lease", parseInt(leaseSeconds));
+    db.set("start", Date.now() / 1000);
+    console.log("Returned");
+    return;
   }
-  const start = await db.get("start")
-  const lease = await db.get("lease")
-  console.log(start, lease)
-  console.log(Date.now()/1000)
-  if (!start || !lease || Date.now()/1000 > start + lease) subscribe()
+  const start = await db.get("start");
+  const lease = await db.get("lease");
+  console.log(start, lease);
+  console.log(Date.now() / 1000);
+  if (!start || !lease || Date.now() / 1000 > start + lease) subscribe();
 }
 
 function subscribe() {
-  console.log("Subscribe() called")
+  console.log("Subscribe() called");
   fetch("https://pubsubhubbub.appspot.com/subscribe", {
     method: "POST",
-    headers: { "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8" },
-    body: `hub.topic=https://www.youtube.com/xml/feeds/videos.xml?channel_id=${process.env.YTID}&hub.callback=${process.env.callback}yt&hub.mode=subscribe&hub.secret=${process.env.YTsecret}`
-  })
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+    },
+    body: `hub.topic=https://www.youtube.com/xml/feeds/videos.xml?channel_id=${process.env.YTID}&hub.callback=${process.env.callback}yt&hub.mode=subscribe&hub.secret=${process.env.YTsecret}`,
+  });
 }
 
 export async function post(ID) {
-  const old = await db.get("ID")
+  const old = await db.get("ID");
   if (ID === old) {
-    console.log("Rejected")
-    return
+    console.log("Rejected");
+    return;
   }
 
   const body = {
     method: "POST",
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      content: `New video uploaded!!! <@&${process.env.YTrole}>\nhttps://www.youtube.com/watch?v=${ID}`
-    })
-  }
-  const result = await fetch(process.env.YTwebhook, body)
+      content: `New video uploaded!!! <@&${process.env.YTrole}>\nhttps://www.youtube.com/watch?v=${ID}`,
+    }),
+  };
+  const result = await fetch(process.env.YTwebhook, body);
   if (result.status == 429) {
-    db.set("failed", ID)
-    exec("kill 1")
+    db.set("failed", ID);
+    exec("kill 1");
   }
-  db.set("ID", ID)
-  db.delete("failed")
+  db.set("ID", ID);
+  db.delete("failed");
 }
 
 export async function check() {
-  const ID = await db.get("failed")
-  if (ID) post(ID)
+  const ID = await db.get("failed");
+  if (ID) post(ID);
 }
